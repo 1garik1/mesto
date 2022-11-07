9637da6a4ef6c806042cf8a222c6ff1c7778d2de
Igor Savelyev <garikmen1997@gmail.com>
2022-11-08 00:44:11 +0300

    refactor: profile feature

---
+++ scripts/scripts.js
@@ -0,0 +1,26 @@
+const openModalBtn = document.querySelector(".profile__button");
+const modal = document.querySelector(".modal");
+const MODAL_ACTIVE_CLASS = "modal__active";
+const modalContent = document.querySelector(".modal__text");
+
+openModalBtn.addEventListener("click", () => {
+    modal.classList.add(MODAL_ACTIVE_CLASS);
+});
+
+modal.addEventListener("click", (event) => {
+    if(!modalContent.contains(event.target)) {
+        modal.classList.remove(MODAL_ACTIVE_CLASS);
+    }
+})
+let formElement = document.querySelector(".form");
+let nameInput = document.querySelector(".form__name");
+let jobInput = document.querySelector(".form__job")
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__about');
+profileName.textContent = nameInput.value;
+profileJob.textContent = jobInput.value;
+
+}
+formElement.addEventListener('submit', formSubmitHandler);
