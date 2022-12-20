6a1578e6a4885c9d65c7a3f908d29261df220afb
Igor Savelyev <garikmen1997@gmail.com>
2022-12-20 12:03:34 +0300

    fix: correct form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,32 @@
-const openModalBtn = document.querySelector(".profile__button");
-const modal = document.querySelector(".modal");
-const MODAL_ACTIVE_CLASS = "modal__active";
-const modalContent = document.querySelector(".modal__text");
+const openModalBtn = document.querySelector(".profile__button"); //задаем кнопку редактирования пользователя
+const modal = document.querySelector(".modal");//задаем всплывающее окно
+const MODAL_ACTIVE_CLASS = "modal_active";//задаем открытое состояние всплывающего окна
+const modalContent = document.querySelector(".modal__text");//задаем данные всплывающего окна(общие)
+const ModalCloseBtn = document.querySelector(".modal__close");//задаем кнопку закрытия всплывающего окна
 openModalBtn.addEventListener("click", () => {
     modal.classList.add(MODAL_ACTIVE_CLASS);
-});
-
+});//действие открытия всплывающего окна, после этого .modal становится видимым
+ModalCloseBtn.addEventListener("click", (event) => {
+    if(!modalContent.contains(event.ModalCloseBtn)) {
+        modal.classList.remove(MODAL_ACTIVE_CLASS);
+    }
+})//задаем действие кнопке закрытия всплывающего окна
 modal.addEventListener("click", (event) => {
     if(!modalContent.contains(event.target)) {
         modal.classList.remove(MODAL_ACTIVE_CLASS);
     }
 
-})
+}) // задаем действие, закрывающее всплывающее окно
 
-let formElement = document.querySelector(".form");
-let nameInput = document.querySelector(".form__name");
-let jobInput = document.querySelector(".form__job")
+let formElement = document.querySelector(".form");//задаем общую форму
+let nameInput = document.querySelector(".form__name");//задаем строку 'name'
+let jobInput = document.querySelector(".form__job");//задаем строку 'job'
 function formSubmitHandler (evt) {
     evt.preventDefault();
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__about');
+let profileName = document.querySelector('.profile__name');//строка ввода name
+let profileJob = document.querySelector('.profile__about');//строка воода job
 profileName.textContent = nameInput.value;
 profileJob.textContent = jobInput.value;
 
-}
-formElement.addEventListener('submit', formSubmitHandler);
+}//задаем функцию отправки на сервер и редактирования профиля
+formElement.addEventListener('submit', formSubmitHandler);//сохраняем значения согласно функции
