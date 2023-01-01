4ec88b4d56eb8191f2cd29f50eacab4d02f2092e
Igor Savelyev <garikmen1997@gmail.com>
2023-01-01 14:17:32 +0300

    fix: popup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,40 @@
-const openModalBtn = document.querySelector(".profile__button"); //задаем кнопку редактирования пользователя
-const modal = document.querySelector(".modal");//задаем всплывающее окно
-const MODAL_ACTIVE_CLASS = "modal_active";//задаем открытое состояние всплывающего окна
-const modalContent = document.querySelector(".modal__text");//задаем данные всплывающего окна(общие)
-const ModalCloseBtn = document.querySelector(".modal__close");//задаем кнопку закрытия всплывающего окна
+const openModalBtn = document.querySelector(".profile__button"); 
+const modal = document.querySelector(".popup");
+const MODAL_ACTIVE_CLASS = "popup_active";
+const modalContent = document.querySelector(".popup__text");
+const ModalCloseBtn = document.querySelector(".popup__close");
+let formElement = document.querySelector(".form");
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__about')
 openModalBtn.addEventListener("click", () => {
     modal.classList.add(MODAL_ACTIVE_CLASS);
-});//действие открытия всплывающего окна, после этого .modal становится видимым
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+
+});
+
+
 ModalCloseBtn.addEventListener("click", (event) => {
-    if(!modalContent.contains(event.ModalCloseBtn)) {
-        modal.classList.remove(MODAL_ACTIVE_CLASS);
+  if(!modalContent.contains(event.ModalCloseBtn)) {
+
+    modal.classList.remove(MODAL_ACTIVE_CLASS);
     }
-})//задаем действие кнопке закрытия всплывающего окна
+})
+
+
 modal.addEventListener("click", (event) => {
-    if(!modalContent.contains(event.target)) {
-        modal.classList.remove(MODAL_ACTIVE_CLASS);
-    }
+  if(!modalContent.contains(event.target)) {
+    modal.classList.remove(MODAL_ACTIVE_CLASS);
+  }
 
-}) // задаем действие, закрывающее всплывающее окно по клику вне модального окна
+}) 
 
-let formElement = document.querySelector(".form");//задаем общую форму
-let nameInput = document.querySelector(".form__name");//задаем строку 'name'
-let jobInput = document.querySelector(".form__job");//задаем строку 'job'
 function formSubmitHandler (evt) {
-    evt.preventDefault();
-let profileName = document.querySelector('.profile__name');//строка ввода name
-let profileJob = document.querySelector('.profile__about');//строка воода job
-profileName.textContent = nameInput.value;
-profileJob.textContent = jobInput.value;
-
-}//задаем функцию отправки на сервер и редактирования профиля
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  modal.classList.remove(MODAL_ACTIVE_CLASS);//закрытие попапа
+}
 formElement.addEventListener('submit', formSubmitHandler);//сохраняем значения согласно функции
