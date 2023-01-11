b0538225b5fa4fb8da3d3889fc6ecd8663a3fd7b
Igor Savelyev <garikmen1997@gmail.com>
2023-01-11 21:57:53 +0300

    refactor: change popup for add pictures

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,33 @@
-const openModalBtn = document.querySelector(".profile__button");//задаем кнопку открытия
-const modal = document.querySelector(".popup");//задаем попап
-const MODAL_ACTIVE_CLASS = "popup_active";//задаем активное состояние попапа
-const modalContent = document.querySelector(".popup__text");//задаем весь контент попапа
-const ModalCloseBtn = document.querySelector(".popup__close");//задаем кнопку закрытия
-let formElement = document.querySelector(".form");//задаем форму
-let nameInput = document.querySelector('.popup__input_type_name');//задаем инпут имени профиля
-let jobInput = document.querySelector('.popup__input_type_job');//задаем инпут рода занятия профиля
-let profileName = document.querySelector('.profile__name');//задаем обозначение имени профиля
-let profileJob = document.querySelector('.profile__about');//задаем обозначение рода деятельности профиля
+//задаем значения попапам
+const popupProfile = document.querySelector('.popup__type_profile');//профиль
+const popupPhoto = document.querySelector('.popup__type_photo');//картинки
+
+//задаем значения кнопкам открытия попапов
+const openModalBtn = document.querySelector('.profile__button');
+const openSecondModalBtn = document.querySelector('.profile__add-button');
+
+//задаем значения кнопкам закрытия попапов
+const closeProfileButton = popupProfile.querySelector('.popup__close');
+const closePhotoButton = popupPhoto.querySelector('.popup__close');
+
+const nameInput = document.querySelector('.popup__input_type_name');//задаем инпут имени профиля
+const jobInput = document.querySelector('.popup__input_type_job');//задаем инпут рода занятия профиля
+const profileName = document.querySelector('.profile__name');//задаем обозначение имени профиля
+const profileJob = document.querySelector('.profile__about');//задаем обозначение рода деятельности профиля
+
+const MODAL_ACTIVE_CLASS = 'popup_active';//задаем активное состояние попапа
+const modalContent = document.querySelector('.popup__text');//задаем весь контент попапа
+const formElementProfile = document.querySelector('.form');//задаем форму
+
+
+
+
+const titleInput = document.querySelector('.popup__input_type_title');
+const linkInput = document.querySelector('.popup__input_type_link');
+
+
 openModalBtn.addEventListener("click", () => {
-    modal.classList.add(MODAL_ACTIVE_CLASS);
+    popupProfile.classList.add(MODAL_ACTIVE_CLASS);
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 
@@ -18,27 +36,39 @@ openModalBtn.addEventListener("click", () => {
 });//открытие попапа и внесение данных профиля в инпуты
 
 
-ModalCloseBtn.addEventListener("click", (event) => {
+closeProfileButton.addEventListener("click", (event) => {
   if(!modalContent.contains(event.ModalCloseBtn)) {
-    modal.classList.remove(MODAL_ACTIVE_CLASS);
+    popupProfile.classList.remove(MODAL_ACTIVE_CLASS);
     }
 })//закрытие попапа через кнопку закрытия
 
 
-modal.addEventListener("click", (event) => {
+modalContent.addEventListener("click", (event) => {
   if(!modalContent.contains(event.target)) {
-    modal.classList.remove(MODAL_ACTIVE_CLASS);
+    popupProfile.classList.remove(MODAL_ACTIVE_CLASS);
   }
 
 })//закрытие попапа по клику вне попапа
 
+openSecondModalBtn.addEventListener("click", () => {
+  popupPhoto.classList.add(MODAL_ACTIVE_CLASS);
+
+
+
+
+});
+closePhotoButton.addEventListener('click', () => {
+  popupPhoto.classList.remove(MODAL_ACTIVE_CLASS);
+})
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   modal.classList.remove(MODAL_ACTIVE_CLASS);//закрытие попапа после изменения профиля на кнопку сабмит
 }
-formElement.addEventListener('submit', formSubmitHandler);//сохраняем значения согласно функции
+formElementProfile.addEventListener('submit', formSubmitHandler);//сохраняем значения согласно функции
+
+
 const cardList = document.querySelector('.elements');
 const cardTemplate = document.querySelector('.elements-template').content;
 const initialCards = [
