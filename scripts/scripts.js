a285d69418f914e2de2af939058e2c20453c5f5d
Igor Savelyev <garikmen1997@gmail.com>
2023-02-10 14:33:38 +0300

    fix: script_1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,5 @@
 //задаем значения попапам
+const popup = document.querySelector('.popup');
 const popupProfile = document.querySelector('.popup_type_profile');//профиль
 const popupPhoto = document.querySelector('.popup_type_photo');//картинки
 const popupZoom = document.querySelector('.popup_type_modal');//zoom
@@ -7,9 +8,7 @@ const openModalBtn = document.querySelector('.profile__button');
 const openSecondModalBtn = document.querySelector('.profile__add-button');
 const picturesOpen = document.querySelector('.elements__image');
 //задаем значения кнопкам закрытия попапов
-const closeProfileButton = popupProfile.querySelector('.popup__close');
-const closePhotoButton = popupPhoto.querySelector('.popup__close');
-//const closeZoombtn = document.querySelector('.popup__close');
+const closeButtons = document.querySelectorAll('.popup__close');
 
 const nameInput = document.querySelector('.popup__input_type_name');//задаем инпут имени профиля
 const jobInput = document.querySelector('.popup__input_type_job');//задаем инпут рода занятия профиля
@@ -18,15 +17,19 @@ const profileJob = document.querySelector('.profile__about');//задаем об
 
 const MODAL_ACTIVE_CLASS = 'popup_active';//задаем активное состояние попапа
 const modalContent = document.querySelector('.popup__text');//задаем весь контент попапа
-const formElementProfile = document.querySelector('.form');//задаем форму
+const formElementProfile = document.querySelector('.form_type_profile');//задаем форму
 
 const container = document.querySelector('.content');
 const cardContainer = container.querySelector('.elements');
-const formCards = document.querySelector('.form__type-photo');
+const formCards = document.querySelector('.form_type_photo');
 const dltBtn = document.querySelector('.elements__delete-btn');
 const mdlPicture = document.querySelector('.popup__picture');
 const mdlTitle = document.querySelector('.popup__picture-title');
-let initialCards = [
+const cardTemplate = document.querySelector('#elements-template').content;
+const title = document.querySelector('.popup__input_type_title');
+const image = document.querySelector('.popup__input_type_src');
+
+const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -52,96 +55,68 @@ let initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-
-initialCards.forEach((element) => {
-  addCard (element.name, element.link);
+//функция добавления карточки
+initialCards.forEach((elements) => {
+  createCard (elements.name, elements.link);
 });
-
-//===============================================
-const titleInput = document.querySelector('.popup__input_type_title');
-const cardInput = document.querySelector('.popup__input_type_src');
-
-function addCard (nameValue, linkValue) {
-
-  const cardTemplate = document.querySelector('#elements-template').content;
+//функция создания карточки
+function createCard (name, link) {
   const cardElement = cardTemplate.querySelector('.elements__element').cloneNode(true);
-  cardElement.querySelector('.elements__title').textContent = nameValue;
-  cardElement.querySelector('.elements__image').src = linkValue;
-
+  cardElement.querySelector('.elements__title').textContent = name;
+  cardElement.querySelector('.elements__image').src = link;
+//лайк картинки
   cardElement.querySelector('.elements__button').addEventListener('click', function (evt) {
     evt.target.classList.toggle('elements__button_active');
       });
-
-  cardElement.querySelector('.elements__delete-btn').addEventListener('click', function () {
-    cardElement.remove();
-  });
-
-  function openZoom () {
-    popupZoom.classList.add(MODAL_ACTIVE_CLASS);
-    mdlTitle.textContent = cardElement.querySelector('.elements__title').textContent;
-    mdlPicture.src = cardElement.querySelector('.elements__image').src;
-  };
-
-
-  function closeZoom () {
-    popupZoom.classList.remove(MODAL_ACTIVE_CLASS);
-   // mdlTitle.textContent = cardElement.querySelector('.elements__title').textContent;
-    //mdlPicture.src = cardElement.querySelector('.elements__image').src;
-  };
-  
-  cardElement.querySelector('.elements__image').addEventListener('click', openZoom);
- popupZoom.querySelector('.popup__close').addEventListener('click', closeZoom);
-  cardContainer.prepend(cardElement);
-}
-
-function formSubmitionHandler (evt) {
-  evt.preventDefault();
-  const title = document.querySelector('.popup__input_type_title');
-  const image = document.querySelector('.popup__input_type_src');
-  addCard(title.value, image.value);
-  title.value = '';
-  image.value = '';
-popupPhoto.classList.remove(MODAL_ACTIVE_CLASS);//закрытие попапа после изменения профиля на кнопку сабмит
+//удаление картинки
+cardElement.querySelector('.elements__delete-btn').addEventListener('click', function () {
+  cardElement.remove();
+});
+//функция увеличения картинки при нажати на нее
+function openZoom () {
+  popupZoom.classList.add(MODAL_ACTIVE_CLASS);
+  mdlTitle.textContent = cardElement.querySelector('.elements__title').textContent;
+  mdlPicture.src = cardElement.querySelector('.elements__image').src;
 };
-//================================================
-
-
-
-function openModal () {
-  popupProfile.classList.add(MODAL_ACTIVE_CLASS);
+//обработчик события увеличения картинки при нажатии
+cardElement.querySelector('.elements__image').addEventListener('click', openZoom);
+//добавление карточек в контейнер
+cardContainer.prepend(cardElement);
 };
-
-function closeModal (event) {
-  if(!modalContent.contains(event.ModalCloseBtn)) {
-    popupProfile.classList.remove(MODAL_ACTIVE_CLASS);
-    }
+//универсальные кнопки открытия/закрытия попапов
+function popupOpend (popup) {
+  popup.classList.add(MODAL_ACTIVE_CLASS);
 };
-
-function formSubmitHandler (evt) {
+function popupClose (popup) {
+  popup.classList.remove(MODAL_ACTIVE_CLASS);
+};
+//изменение профиля
+function editProfile () {
+  popupOpend(popupProfile);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+};
+//сохранение изменений профиля
+function handleProfileFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupProfile.classList.remove(MODAL_ACTIVE_CLASS);//закрытие попапа после изменения профиля на кнопку сабмит
-
+  popupProfile.classList.remove(MODAL_ACTIVE_CLASS);
 };
-//==================================================
-function openSecondModal () {
-  popupPhoto.classList.add(MODAL_ACTIVE_CLASS);
-
+//сохранение изменений и добавление карточки
+function handleAddFormSubmit (evt) {
+  evt.preventDefault();
+  createCard(title.value, image.value);
+  title.value = ' ';
+  image.value = ' ';
+popupPhoto.classList.remove(MODAL_ACTIVE_CLASS);
 };
-
-function closeSecondModal () {
-  const titleInput = document.querySelector('.popup__input_type_title').value;
-  const cardInput = document.querySelector('.popup__input_type_src').value;
-  popupPhoto.classList.remove(MODAL_ACTIVE_CLASS);
-};
-//===========================================================
-
-
-//===========================================================
-openModalBtn.addEventListener('click', openModal);
-closeProfileButton.addEventListener('click', closeModal);
-formElementProfile.addEventListener('submit', formSubmitHandler);
-openSecondModalBtn.addEventListener('click', openSecondModal);
-closePhotoButton.addEventListener('click', closeSecondModal);
-formCards.addEventListener('submit', formSubmitionHandler);
+//обработчики событий
+openModalBtn.addEventListener('click', editProfile);
+openSecondModalBtn.addEventListener('click', () => { popupOpend(popupPhoto) });
+closeButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => {popupClose(popup)});
+});
+formElementProfile.addEventListener('submit', handleProfileFormSubmit);
+formCards.addEventListener('submit', handleAddFormSubmit);
